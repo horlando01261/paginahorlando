66341cb5920e6e95e84646300bc51efd5b9fed92
horlando01261 <horlandomar00@gmail.com>
2025-04-15 08:36:40 -0300

    Add files via upload

---
+++ horl.js
@@ -0,0 +1,26 @@
+ // Inicializar Slick Carousel
+$(document).ready(function() {
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true
+    });
+});
+// Inicializar Slick Carousel
+$(document).ready(function() {
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000, // Velocidad más rápida (1 segundo)
+        arrows: true
+    });
+});
